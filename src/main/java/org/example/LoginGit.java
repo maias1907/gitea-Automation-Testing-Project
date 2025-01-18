ceeb3917219ed1140da02b466b0098f724f14cb7
maias1907 <144439334+maias1907@users.noreply.github.com>
2025-01-18 21:57:44 +0200

    webFormTest

--- src/main/java/org/example/LoginGit.java
+++ src/main/java/org/example/LoginGit.java
@@ -2,21 +2,19 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class LoginGit extends LoadableComponent<LoginGit> {
     private WebDriver driver;
-    private final String baseURL=" https://6c37-2a06-c701-78d3-4f00-adba-8056-f580-4086.ngrok-free.app ";
-
-    private By userNameFieldBy = By.id("user_name");
+    private final String baseURL="https://668b-2a06-c701-78d3-4f00-f943-2c4c-636d-e810.ngrok-free.app";
+    private final String baseURL1="http://localhost:3000";
+    private By userNameFieldBy = By.cssSelector("input[name=\"user_name\"]");
     public By passwordFieldBy = By.cssSelector("input[name=\"password\"]");
     private By signinButtonBy = By.cssSelector("button.ui.primary.button.tw-w-full");
 
@@ -25,35 +23,15 @@ public class LoginGit extends LoadableComponent<LoginGit> {
         PageFactory.initElements(driver,this);
 
     }
-    @Override
-    protected void load() {
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(baseURL+"/user/login");
-        System.out.println(driver.getCurrentUrl());
-
-    }
     public  HomePageGit loginAsValidUser(String userName, String password) {
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
-
-        // Wait until the username field is present and visible
-        WebElement userNameField = wait.until(d -> d.findElement(userNameFieldBy));
-        userNameField.sendKeys(userName);
-        // Wait until the password field is present and visible
-        WebElement passwordField = wait.until(d -> d.findElement(passwordFieldBy));
-        passwordField.sendKeys(password);
-        // Wait until the sign-in button is clickable
-        WebElement signinButton = wait.until(d -> d.findElement(signinButtonBy));
-
-        // Interact with the fields
-
-
-        signinButton.click();
+        driver.findElement(userNameFieldBy).sendKeys(userName);
+        driver.findElement(passwordFieldBy).sendKeys(password);
+        driver.findElement(signinButtonBy).click();
 
         return new HomePageGit(driver);
     }
-  /*  public LoginGit loginWithInvalidCredentials(String userName, String password){
+    public LoginGit loginWithInvalidCredentials(String userName, String password){
         driver.findElement(userNameFieldBy).clear();
         driver.findElement(userNameFieldBy).sendKeys(userName);
         driver.findElement(passwordFieldBy).clear();
@@ -64,12 +42,20 @@ public class LoginGit extends LoadableComponent<LoginGit> {
     public boolean isLoginFailed(){
 
         return driver.getTitle().contains("Sign In");
-    }*/
+    }
 
+    @Override
+    protected void load() {
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(baseURL+"/user/login");
+        System.out.println(driver.getCurrentUrl());
 
+    }
 
     @Override
     protected void isLoaded() throws Error {
+        assertTrue(driver.getTitle().contains("Sign In"));
+
     }
 }
 
